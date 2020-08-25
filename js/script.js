1aab0f2e8c794ac153a710f2512f0f26fd176694
mohak92 <tamhane.mohak@gmail.com>
2020-08-25 13:01:44 -0400

    autoplay

--- js/script.js
+++ js/script.js
@@ -36,7 +36,9 @@ $(document).ready(function() {
 
 	$('.owl-carousel').owlCarousel({
 	    loop:true,
-	    items: 4,
+		items: 4,
+		autoplay:true,
+		autoplayTimeout:1000,
 	    responsive:{
 	        0:{
 	            items:1
